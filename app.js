1ee801176cee4de53f8f34e9f5b7c19eb7d2b73a
Agustin Costa Ditarcio <agustin.costa.995@gmail.com>
2022-08-28 02:12:09 -0300

    Ultima consigna

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
 app.get("/", function(req,res) {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
+
+app.get("/register", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
+app.get("/login", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
